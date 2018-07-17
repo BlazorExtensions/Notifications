6b91c81127619e40cfadb6c1b22e0875106d6b31
Gutemberg Ribeiro <gutemberg@outlook.com>
2018-07-17 10:00:53 -0300

    Initial project structure

---
+++ src/Blazor.Extensions.Notifications.JS/src/Initialize.ts
@@ -0,0 +1,8 @@
+import './GlobalExports';
+
+// import { HubConnectionManager } from './HubConnectionManager';
+
+// "use strict";
+// HubConnectionManager.initialize();
+
+//TODO: Import all your .TS here and initialize it. This script will be called when the package is referenced and app start.
