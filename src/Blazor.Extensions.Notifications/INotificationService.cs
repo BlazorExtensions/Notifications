5da77bff3f93135c078b12d6f271e59e624032b7
Mikart <mikart143@gmail.com>
2020-04-13 17:11:16 +0200

    Updated libs,  fixed notification service,  updated test project

--- src/Blazor.Extensions.Notifications/INotificationService.cs
+++ src/Blazor.Extensions.Notifications/INotificationService.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 
+
 namespace Blazor.Extensions
 {
     public interface INotificationService
@@ -8,19 +9,20 @@ namespace Blazor.Extensions
         /// Checks if the Notifications' API is Support by the browser.
         /// </summary>
         /// <returns></returns>
-        Task<bool> IsSupportedByBrowserAsync();
+        ValueTask<bool> IsSupportedByBrowserAsync();
         /// <summary>
         /// Request the user for his permission to send notifications.
         /// </summary>
         /// <returns></returns>
         Task<PermissionType> RequestPermissionAsync();
+
         /// <summary>
         /// Create a Notification with <seealso cref="NotificationOptions"/>
         /// </summary>
         /// <param name="title"></param>
         /// <param name="options"></param>
         /// <returns></returns>
-        Task CreateAsync(string title, NotificationOptions options);
-        Task CreateAsync(string title, string description, string iconUrl);
+        ValueTask<string> CreateAsync(string title, NotificationOptions options);
+        ValueTask<string> CreateAsync(string title, string description, string iconUrl);
     }
 }
